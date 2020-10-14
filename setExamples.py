523031a43bb6eee4d8d72ab786955fc5e987b671
yagmurkaraman <yagkaraman@gmail.com>
2020-10-14 22:11:56 +0300

    set examples

---
+++ setExamples.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 22:03:20 2020
+
+@author: yagmurkaraman
+"""
+
+####################    DATA STRUCTURES - SETS    ####################
+### out of order
+### unique elements
+### changeable
+
+setEx = set() #creates a set
+
+listEx = [1, "hello", "yagmur", 9]
+setEx = set(listEx)
+print(setEx) #prints {1, 'hello', 'yagmur', 9}
+
+
+tupleEx = (1, "hello", "yagmur", 9)
+setEx = set(tupleEx)
+print(setEx) #prints {1, 'hello', 'yagmur', 9}
+
+
+
